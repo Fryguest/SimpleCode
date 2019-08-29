46aa6cd60e49a44ffa9b34600fa5a4e528a330fc
fry_guest <fryguestcc@gmail.com>
2019-08-29 21:12:59 +0800

    add 977

---
+++ 977A Wrong Subtraction.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n,k;
+    while (~scanf("%d%d", &n,&k))
+    {
+        while (k--)
+        {
+            if (n%10) n--;
+            else n/=10;
+        }
+        printf("%d\n",n);
+    }
+    return 0;
+}
