ec24024098760a6657bde85c7209bb014d6d0e04
fry_guest <fryguestcc@gmail.com>
2019-09-10 15:32:36 +0800

    add 935A

---
+++ 935A Fafa and his Company.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    while (cin>>n)
+    {
+        int ans=0;
+        for (int i=2;i<=n;i++)
+        {
+            if (n%i==0) ans++;
+        }
+        cout<<ans<<endl;
+    }
+    return 0;
+}
