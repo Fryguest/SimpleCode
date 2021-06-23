6ad705800a19653023202da97fb8177a82cce41c
fry_guest <fryguestcc@gmail.com>
2021-06-23 14:24:46 +0800

    up

---
+++ Codeforces/1520B Ordinary Numbers.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int t;
+	cin>>t;
+	while (t--)
+	{
+		long long n;
+		cin>>n;
+		int ans = 0;
+		for (int i = 1; i <=9; i++)
+		{
+			long long x = i;
+			while (x <= n)
+			{
+				ans++;
+				x = x  * 10 + i;
+			}
+		}
+		cout<<ans<<endl;
+	}
+	return 0;
+}
