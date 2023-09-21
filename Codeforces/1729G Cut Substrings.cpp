6bf36467a0284b60640907dec06061a47acb9b9b
fry_guest <fryguestcc@gmail.com>
2023-09-21 17:28:39 +0800

    Update

--- Codeforces/1729G Cut Substrings.cpp
+++ Codeforces/1729G Cut Substrings.cpp
@@ -1,81 +1,88 @@
 #include <bits/stdc++.h>
 using namespace std;
-const int mod = 1e9+7;
+const int mod=1e9+7;
 const int N=505;
 bool v[N];
-int dp[N][N];
+int dp[N][N][2];
 bool cal(const string& a, const string& b, int x)
 {
-    if (x+b.size()>a.size()) return false;
-    for (int i=0;i<b.size();i++) if (b[i]!=a[x+i]) return false;
+    if(x<b.size()-1) return false;
+    for(int i=0;i<b.size();i++)if (b[i]!=a[x-b.size()+i+1]) return false;
     return true;
 }
 int main()
 {
     int t;
     cin>>t;
-    string a,b;
+    string s1,s2;
     while(t--)
     {
-        cin>>a>>b;
+        cin>>s1>>s2;
         memset(v,0,sizeof(v));
-        for (int i=0;i<a.size();i++) v[i]=cal(a,b,i);
+        for (int i=0;i<s1.size();i++) v[i]=cal(s1,s2,i);
+//        for (int i=0;i<s1.size();i++) cout<<v[i]<<" ";cout<<endl<<endl;
         memset(dp,0,sizeof(dp));
-        int I=a.size(), J=a.size()/b.size();
-        for (int i=0;i<I;i++) dp[i][0]=1;
-        for (int j=1;j<=J;j++)
+        for (int j=0;j<=s1.size()/s2.size();j++)
         {
-            for (int i=0;i<I;i++)
+            for (int i=0;i<s1.size();i++)
             {
                 if (v[i])
                 {
-                    if (i>=b.size())
+                    if (j==0)
                     {
-                        dp[i][j]=(dp[i-1][j]+dp[i-b.size()][j-1])%mod;
+                        dp[i][j][0]=0;
+                        dp[i][j][1]=0;
                     }
                     else
                     {
-                        if (j==1)
+                        dp[i][j][0]=0;
+                        for (int k=i-s2.size()+1;k<i;k++)
                         {
-                            if(i>0) dp[i][j]=1+dp[i-1][j];
-                            else dp[i][j]=1;
+                            dp[i][j][0]=(dp[i][j][0]+dp[k][j][1])%mod;
                         }
-                        else
-                        {
-                            dp[i][j]=0;
-                        }
+                        if (i>=s2.size()) dp[i][j][1]=(dp[i-s2.size()][j-1][0]+dp[i-s2.size()][j-1][1])%mod;
+                        else if (j==1) dp[i][j][1]=1;
+                        else dp[i][j][1]=0;
                     }
                 }
                 else
                 {
-                    if (i==0) dp[i][j]=0;
-                    else dp[i][j]=dp[i-1][j];
-                }
-            }
-        }
-        int ans1=-1, ans2=-1;
-        for (int j=0;j<=J;j++)
-        {
-            for (int i=0;i<I;i++)
-            {
-                cout<<dp[i][j]<<" ";
-            }cout<<endl;
-        }
-        for (int i=0;i<I;i++)
-        {
-            for (int j=0;j<=J;j++)
-            {
-                if (dp[i][j]>0)
-                {
-                    if (j>ans1 || (j==ans1 && dp[i][j]>ans2))
+                    dp[i][j][1]=0;
+                    if (i==0)
                     {
-                        ans1=j;
-                        ans2=dp[i][j];
+                        if (j==0)dp[i][j][0]=1;
+                        else dp[i][j][0]=0;
                     }
+                    else dp[i][j][0]=(dp[i-1][j][0]+dp[i-1][j][1])%mod;
                 }
             }
         }
-        cout<<ans1<<" "<<ans2<<endl;
+        for (int j=0;j<=s1.size()/s2.size();j++)
+        {
+            if (dp[s1.size()-1][j][0]+dp[s1.size()-1][j][1])
+            {
+                cout<<j<<" "<<(dp[s1.size()-1][j][0]+dp[s1.size()-1][j][1])%mod<<endl;
+                break;
+            }
+        }
+//        for (int j=0;j<=s1.size()/s2.size();j++)
+//        {
+//            for (int i=0;i<s1.size();i++)
+//            {
+//                cout<<dp[i][j][0]+dp[i][j][1]<<" ";
+//            }cout<<endl;
+//        }
+//        cout<<endl;
+
+//        for (int j=0;j<=s1.size()/s2.size();j++)
+//        {
+//            for (int i=0;i<s1.size();i++)
+//            {
+//                cout<<dp[i][j][1]<<" ";
+//            }cout<<endl;
+//        }
+//        cout<<endl;
     }
     return 0;
 }
+
